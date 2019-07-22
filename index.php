a7cadc96df2e477885017da5626f502f6ff5743c
LutenkoMikhail <lamprologus2011@gmail.com>
2019-07-22 15:15:40 +0300

    Optimized function arrayIntersect

--- index.php
+++ index.php
@@ -3,16 +3,15 @@ ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-$initialValuesArrayOne = [0, -1, -2, 3, 4, -5, 6, -7, 88, 9, -10];
-$initialValuesArrayTwo = [0, 1, -2, 3, 4, -5, 6, -7, 77, 9];
+$initialValuesArrayOne = [0, -1, -2, 3, 4, -5, 6, -7, 88, 9, -10, 22];
+$initialValuesArrayTwo = [0, 1, 2, 3, 4, -5, 6, -7, 77, 9, 10, 11, 12, 23];
 $resultArray = [];
 $countNumbersArray = 0;
-$iterator = 0;
 
 
-echo "<h1>" . "Dev-Pro.net" . "</h1>";
-echo "<h3>" . "ТЗ.№ 4  " . "</h3>";
-echo "<h3>" . "Функция нахождения в массивах все пересекающие значения. " . "</h3>";
+echo "<h1> Dev-Pro.net</h1>";
+echo "<h3>ТЗ.№ 4  </h3>";
+echo "<h3>Функция нахождения в массивах все пересекающие значения. </h3>";
 
 /**
  * The function of finding in the arrays is all crossing values.
@@ -23,28 +22,13 @@ echo "<h3>" . "Функция нахождения в массивах все п
 function arrayIntersect(array $arrayOne, array $arrayTwo): array
 {
     $resultArray = [];
-    $iteratorOne = 0;
-    $iteratorTwo = 0;
-    $countArrayOne = 0;
-    $countArrayTwo = 0;
-    $readNumberOne = 0;
-    $readNumberTwo = 0;
-
-    $countArrayOne = count($arrayOne);
-    $countArrayTwo = count($arrayTwo);
-
-    if ($countArrayOne !== 0 && $countArrayTwo !== 0) {
-        while ($iteratorOne < $countArrayOne) {
-            $readNumberOne = $arrayOne[$iteratorOne];
-            $iteratorTwo = 0;
-            while ($iteratorTwo < $countArrayTwo) {
-                $readNumberTwo = $arrayTwo[$iteratorTwo];
-                if ($readNumberOne === $readNumberTwo) {
-                    $resultArray[] = $readNumberOne;
+    if (count($arrayOne) !== 0 && count($arrayTwo) !== 0) {
+        foreach ($arrayOne as $elementArrayOne) {
+            foreach ($arrayTwo as $elementArrayTwo) {
+                if ($elementArrayOne === $elementArrayTwo) {
+                    $resultArray[] = $elementArrayOne;
                 }
-                $iteratorTwo++;
             }
-            $iteratorOne++;
         }
     }
     return $resultArray;
@@ -57,10 +41,10 @@ $countNumbersArray = count($resultArray);
 if ($countNumbersArray === 0) {
     die("Пересекающиеся значения в 2-х массивах не найдены!");
 }
-echo "В исходных массивах было найдено {$countNumbersArray} пересекающихся значений :" . "<br>";
-while ($iterator < $countNumbersArray) {
-    echo $resultArray[$iterator] . "<br>";
-    $iterator++;
+
+echo "В исходных массивах было найдено {$countNumbersArray} пересекающихся значений :<br>";
+foreach ($resultArray as $elemensArray) {
+    echo $elemensArray . "<br>";
 }
 
 
